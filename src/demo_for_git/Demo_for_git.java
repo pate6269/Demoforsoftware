b55a6177f938f68bfa3bdd2dac4ec8becdbc7c1d
pate6269 <pate6269@sheridancollege.ca>
2025-01-17 12:19:51 -0500

    making change

--- src/demo_for_git/Demo_for_git.java
+++ src/demo_for_git/Demo_for_git.java
@@ -15,7 +15,7 @@ public class Demo_for_git {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World");
+        // making chnage 
     }
     
 }
